15ce55b1a573219342558f6f71e8927f8a3b55d9
lucioventurim <34462880+lucioventurim@users.noreply.github.com>
2021-02-06 13:15:57 -0300

    Create perform.py

---
+++ codigo/perform.py
@@ -0,0 +1,21 @@
+from mfpt import MFPT
+from paderborn import Paderborn
+from experimenter import Experimenter
+from classifiers import Classifiers, Scoring
+
+def main():
+    debug = 0
+    sample_size = 8192
+    
+    database = MFPT(debug=debug)
+    #database = Paderborn(debug=debug)
+
+    database_acq = database.load()
+    #print(database_acq)
+
+    database_exp = Experimenter(database_acq, sample_size)
+    database_exp.perform(Classifiers(), Scoring())
+
+
+if __name__ == "__main__":
+  main()
