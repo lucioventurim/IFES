81431e53c9e4ba5a9ce644f4a5fec416f27214e9
lucioventurim <34462880+lucioventurim@users.noreply.github.com>
2021-02-26 00:05:30 -0300

    Create auto_random_forest.py

---
+++ codigo_novo/classification_models/auto_random_forest.py
@@ -0,0 +1,25 @@
+# Random Forest
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from features_extractors.statistical import Statistical
+
+
+def instantiate_auto_random_forest():
+
+    rf = Pipeline([
+        ('FeatureExtraction', Statistical()),
+        ('scaler', StandardScaler()),
+        ('rf', RandomForestClassifier()),
+    ])
+
+    parameters_rf = {
+        "rf__max_features": [1, 5, 10],
+        "rf__n_estimators": [10, 100, 200],
+    }
+
+    rf = GridSearchCV(rf, parameters_rf)
+
+    return rf
