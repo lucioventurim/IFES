3c4e39eb4d02c0b8e3852b301cb0c4f97f6adb09
lucioventurim <34462880+lucioventurim@users.noreply.github.com>
2021-03-01 23:03:32 -0300

    Update main.py

--- codigo_novo/main.py
+++ codigo_novo/main.py
@@ -4,6 +4,7 @@ from classification_models import auto_knn, auto_random_forest
 #from classification_models import auto_cnn, auto_alexnet
 from sklearn.metrics import accuracy_score, f1_score
 from statistics import mean, stdev
+from utils import pickle_utils
 
 import numpy as np
 
@@ -20,36 +21,29 @@ def main():
             #('AlexNet', auto_alexnet.instantiate_auto_alexnet())
             ]
 
-    print("### Stratified Kfold ###")
-    for clf in clfs:
-        accuracy = []
-        f1_macro = []
-        print(clf[0])
-        for X_train, y_train, X_test, y_test in mfpt.stratifiedkfold():
-            clf[1].fit(X_train, y_train)
-            y_pred = clf[1].predict(X_test)
-            accuracy.append(accuracy_score(y_test, y_pred))
-            f1_macro.append(f1_score(y_test, y_pred, average='macro'))
-        print("Accuracy: ", accuracy, "Mean: ", mean(accuracy), "Std: ", stdev(accuracy))
-        print("F1 Macro: ", f1_macro, "Mean: ", mean(f1_macro), "Std: ", stdev(f1_macro))
+    splits = [('StratifiedKfold', 'stratifiedkfold'),
+              ('GroupKfold', 'groupkfold_custom')]
 
-    print("### Custom Group Kfold ###")
-    for clf in clfs:
-        accuracy = []
-        f1_macro = []
-        print(clf[0])
-        for X_train, y_train, X_test, y_test in mfpt.groupkfold_custom():
-            X_train = np.array(X_train)
-            y_train = np.array(y_train)
-            X_test = np.array(X_test)
-            y_test = np.array(y_test)
+    results = {}
+    for folds in splits:
+        print("### ", folds[0], " ###")
+        for clf in clfs:
+            accuracy = []
+            f1_macro = []
+            print(clf[0])
+            fold_number = 1
+            for X_train, y_train, X_test, y_test in getattr(mfpt, folds[1])():
+                clf[1].fit(X_train, y_train)
+                y_pred = clf[1].predict(X_test)
+                accuracy.append(accuracy_score(y_test, y_pred))
+                f1_macro.append(f1_score(y_test, y_pred, average='macro'))
+                results[folds[0]+"_"+clf[0]+"_"+str(fold_number)] = [np.array(y_test), y_pred]
+                fold_number = fold_number + 1
+            print("Accuracy: ", accuracy, "Mean: ", mean(accuracy), "Std: ", stdev(accuracy))
+            print("F1 Macro: ", f1_macro, "Mean: ", mean(f1_macro), "Std: ", stdev(f1_macro))
 
-            clf[1].fit(X_train, y_train)
-            y_pred = clf[1].predict(X_test)
-            accuracy.append(accuracy_score(y_test, y_pred))
-            f1_macro.append(f1_score(y_test, y_pred, average='macro'))
-        print("Accuracy: ", accuracy, "Mean: ", mean(accuracy), "Std: ", stdev(accuracy))
-        print("F1 Macro: ", f1_macro, "Mean: ", mean(f1_macro), "Std: ", stdev(f1_macro))
+    saved_results = pickle_utils.save_pickle(results)
+    pickle_utils.load_pickle(saved_results)
 
 
 if __name__ == "__main__":
