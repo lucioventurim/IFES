a8418879b1bc9be206587d34f45bad489984e3ed
lucioventurim <34462880+lucioventurim@users.noreply.github.com>
2021-03-07 23:28:11 -0300

    Create metrics.py

---
+++ experiments/utils/metrics.py
@@ -0,0 +1,33 @@
+
+from utils.persist_results import load_results
+from statistics import mean, stdev
+from sklearn.metrics import accuracy_score, f1_score
+
+
+def scores(file):
+
+    results = load_results(file)
+
+    n_folds = 0
+    for fold in results:
+        current_fold = fold[3]
+        if current_fold > n_folds:
+            n_folds = current_fold
+
+    accuracy = []
+    f1_macro = []
+    for fold in results:
+        current_fold = fold[3]
+        if current_fold == n_folds:
+            accuracy.append(accuracy_score(fold[4], fold[5]))
+            f1_macro.append(f1_score(fold[4], fold[5], average='macro'))
+            print("## Splitting Strategy: ", fold[1], "##")
+            print("# Classification Model: ", fold[2], "#")
+            print("Accuracy: ", accuracy, "Mean: ", mean(accuracy), "Std: ", stdev(accuracy))
+            print("F1 Macro: ", f1_macro, "Mean: ", mean(f1_macro), "Std: ", stdev(f1_macro))
+            print()
+            accuracy = []
+            f1_macro = []
+        else:
+            accuracy.append(accuracy_score(fold[4], fold[5]))
+            f1_macro.append(f1_score(fold[4], fold[5], average='macro'))
