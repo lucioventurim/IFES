025287072f5aee61ed201c63d1e7051635450c92
lucioventurim <34462880+lucioventurim@users.noreply.github.com>
2021-03-07 23:27:35 -0300

    Update persist_results.py

--- experiments/utils/persist_results.py
+++ experiments/utils/persist_results.py
@@ -1,7 +1,8 @@
 
-
 from datetime import datetime
 import csv
+import ast
+
 
 def save_results(results):
 
@@ -9,18 +10,39 @@ def save_results(results):
     date_time = now.strftime("%Y.%m.%d_%H.%M.%S")
     result_file_name = date_time + ".csv"
 
-    with open(result_file_name, 'w') as f:
-        for key in results.keys():
-            f.write("%s,%s\n" % (key, results[key]))
+    with open(result_file_name, 'w', newline="") as csvfile:
+        fieldnames = ['dataset', 'split', 'classifier', 'fold', 'y_actual', 'y_pred', 'y_proba']
+        writer = csv.writer(csvfile)
+        writer.writerow(fieldnames)
+        writer.writerows(results)
 
     return result_file_name
 
+
 def load_results(file):
 
-    #a_csv_file = open(file, "r")
-    #dict_reader = csv.DictReader(a_csv_file)
+    results = []
+    with open(file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+                pass
+            else:
+                dataset = row[0]
+                split = row[1]
+                classifier = row[2]
+                fold = int(row[3])
+                y_actual = ast.literal_eval(row[4])
+                if len(y_actual) == 1:
+                    y_actual = list(y_actual[0])
+                y_pred = ast.literal_eval(row[5])
+                if len(y_pred) == 1:
+                    y_pred = list(y_pred[0])
+                y_proba = row[6]
+                row_results = [dataset, split, classifier, fold, y_actual, y_pred, y_proba]
+                results.append(row_results)
+                line_count += 1
 
-    #ordered_dict_from_csv = list(dict_reader)[0]
-    #dict_from_csv = dict(ordered_dict_from_csv)
-
-    print(file)
+    return results
