bffd538c649182d00756e9d1fd517acb141a0532
Lucio <lucioventurim@hotmail.com>
2021-04-18 22:58:53 -0300

    Changed CNN model.

--- experiments/main.py
+++ experiments/main.py
@@ -51,20 +51,20 @@ def main():
     dname = os.path.dirname(abspath)
     os.chdir(dname)
 
-    clfs = [#('K-Nearest Neighbors', auto_knn.instantiate_auto_knn()),
+    clfs = [('K-Nearest Neighbors', auto_knn.instantiate_auto_knn()),
             #('Random Forest', auto_random_forest.instantiate_auto_random_forest()),
             ('CNN', auto_cnn.instantiate_auto_cnn()),
             #('Logistic Regression', auto_lr.instantiate_auto_lr()),
             ]
 
     splits = [('Kfold', 'kfold'),
-              #('StratifiedKfold', 'stratifiedkfold'),
-              #('GroupKfold by Acquisition', 'groupkfold_acquisition'),
-              #('GroupKfold by Settings', 'groupkfold_settings'),
-              #('GroupKfold by Bearings', 'groupkfold_bearings'),
+              ('StratifiedKfold', 'stratifiedkfold'),
+              ('GroupKfold by Acquisition', 'groupkfold_acquisition'),
+              ('GroupKfold by Settings', 'groupkfold_settings'),
+              ('GroupKfold by Bearings', 'groupkfold_bearings'),
              ]
 
-    dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings_min.csv", n_aquisitions=1))
+    dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings_debug.csv", n_aquisitions=4))
     experimenter(dataset, clfs, splits)
 
 
