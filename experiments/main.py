f90ee2e22f36f0a68dd3196d86a4d724e18d2a9e
Lucio <lucioventurim@hotmail.com>
2021-06-07 20:45:46 -0300

    Adequations for FaultNet experiment with kfold.

--- experiments/main.py
+++ experiments/main.py
@@ -36,23 +36,25 @@ def run_train_test(classifier, X_train, y_train, X_test):
     return y_pred, y_proba
 
 @timer
-def experimenter(dataset, clfs, splits):
+def experimenter(dataset, clfs, splits, n_experiments):
     print("### Dataset: ", dataset[0], "###")
     write_in_file("execution_time", f"{dataset[0]}\n")
     dataset[1].download()
     results = []
     print("Performing Experiments.")
-    for folds in splits:
-        for clf in clfs:
-            fold_number = 1
-            print(folds[0], clf[0])
-            write_in_file("execution_time", f"{folds[0]} - {clf[0]}\n")
-            for X_train, y_train, X_test, y_test in getattr(dataset[1], folds[1])():
-                write_in_file("execution_time", f"{fold_number}: ")
-                print("fold_number: ", fold_number)
-                y_pred, y_proba = run_train_test(clf[1], X_train, y_train, X_test)
-                results.append([dataset[0], folds[0], clf[0], fold_number, y_test, y_pred, y_proba])
-                fold_number = fold_number + 1
+    for i in range(1, n_experiments+1):
+        print("## Experiment N.", i, "##")
+        for folds in splits:
+            for clf in clfs:
+                fold_number = 1
+                print(folds[0], clf[0])
+                write_in_file("execution_time", f"{folds[0]} - {clf[0]}\n")
+                for X_train, y_train, X_test, y_test in getattr(dataset[1], folds[1])():
+                    write_in_file("execution_time", f"{fold_number}: ")
+                    print("fold_number: ", fold_number)
+                    y_pred, y_proba = run_train_test(clf[1], X_train, y_train, X_test)
+                    results.append([dataset[0], folds[0], clf[0], fold_number, y_test, y_pred, y_proba])
+                    fold_number = fold_number + 1
     saved_results = persist_results.save_results(results)
     metrics.scores(saved_results)
 
@@ -76,8 +78,9 @@ def main():
               #('GroupKfold by Bearings', 'groupkfold_bearings'),
              ]
 
-    dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings_min.csv", n_aquisitions=1))
-    experimenter(dataset, clfs, splits)
+    n_experiments = 10
+    dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings_paper.csv", n_aquisitions=20))
+    experimenter(dataset, clfs, splits, n_experiments)
 
 
 if __name__ == "__main__":
