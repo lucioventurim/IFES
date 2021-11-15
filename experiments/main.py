7d162cd893698970dc17e82b561e706f54b677ad
Lucio <lucioventurim@hotmail.com>
2021-11-14 22:53:04 -0300

    Paderborn FaultNet test

--- experiments/main.py
+++ experiments/main.py
@@ -85,7 +85,7 @@ def main():
 
     n_experiments = 1
     #dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings.csv", n_aquisitions=20))
-    dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings_debug.csv", n_aquisitions=1))
+    dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings_debug.csv", n_aquisitions=4))
     #dataset = ('MFPT', MFPT())
     #dataset = ('Ottawa', Ottawa())
     #dataset = ('Ottawa', Ottawa(downsample=True))
