304ef32723b31004bb60f4344676149f71b3d343
Lucio <lucioventurim@hotmail.com>
2021-11-19 06:58:55 -0300

    Paderborn test

--- experiments/main.py
+++ experiments/main.py
@@ -32,6 +32,7 @@ def timer(func):
 
 @timer
 def run_train_test(classifier, X_train, y_train, X_test):
+    keras.backend.clear_session()
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
     y_proba = classifier.predict_proba(X_test)
@@ -58,7 +59,7 @@ def experimenter(dataset, clfs, splits, n_experiments):
                     y_pred, y_proba = run_train_test(clf[1], X_train, y_train, X_test)
                     results.append([dataset[0], folds[0], clf[0], fold_number, y_test, y_pred, y_proba])
                     fold_number = fold_number + 1
-                keras.backend.clear_session()
+                #keras.backend.clear_session()
         saved_results = persist_results.save_results(results)
         metrics.scores(saved_results)
 
@@ -86,8 +87,8 @@ def main():
              ]
 
     n_experiments = 9
-    dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings.csv", n_aquisitions=20))
-    #dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings_min.csv", n_aquisitions=4))
+    #dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings.csv", n_aquisitions=20))
+    dataset = ('Paderborn', Paderborn(bearing_names_file="paderborn_bearings_min.csv", n_aquisitions=4))
     #dataset = ('MFPT', MFPT())
     #dataset = ('Ottawa', Ottawa())
     #dataset = ('Ottawa', Ottawa(downsample=True))
